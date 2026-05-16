c3e2d7149ce4d49d592d73b60d68407af8bf8758
brobro332 <332bro@naver.com>
2026-05-16 20:45:54 +0900

    chore: flyway 및 JPA 적용 확인을 위한 member 엔티티 추가

---
+++ src/main/java/xyz/datt/member/entity/MemberRole.java
@@ -0,0 +1,13 @@
+package xyz.datt.member.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberRole {
+    USER("일반 사용자"),
+    ADMIN("관리자");
+
+    private final String description;
+}
